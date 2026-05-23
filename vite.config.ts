5776a4d76f92c357676bb673bfb018bcf3bc56ca
Phichayapa Soranaraksopon <101024240+phiiim@users.noreply.github.com>
2026-05-23 23:56:17 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/phiiim/',
+  base: '/fin_goal/',
   plugins: [react()],
   resolve: {
     alias: { '@': path.resolve(__dirname, './src') }
